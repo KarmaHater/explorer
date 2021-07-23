8fa8cf894b788a6291944b4cee763bf0795b8d33
Adnard-kod <andra.lally@gmail.com>
2021-07-23 12:38:10 -0600

    rename fun

--- src/ui-library/SearchBar/index.js
+++ src/ui-library/SearchBar/index.js
@@ -2,15 +2,14 @@ import React, { useCallback, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
 const SearchBar = ({ onSubmit, onClear }) => {
+  const [searchState, setSearchState] = useState({ value: "" });
   const topic = useSelector(({ topics }) => topics.topic);
 
   useEffect(() => {
     setSearchState({ value: "" });
   }, [topic]);
 
-  const [searchState, setSearchState] = useState({ value: "" });
-
-  const handleChangeCB = useCallback((event) => {
+  const handleOnChangeCB = useCallback((event) => {
     setSearchState({ value: event.target.value });
   }, []);
 
@@ -35,7 +34,7 @@ const SearchBar = ({ onSubmit, onClear }) => {
           type="text"
           placeholder="Topic"
           value={searchState.value}
-          onChange={handleChangeCB}
+          onChange={handleOnChangeCB}
         />
         <input type="submit" value={"Search"} onClick={handleOnSubmitCB} />
         <input type="submit" value={"Clear"} onClick={handleOnClearCB} />
